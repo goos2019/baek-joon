da5028b725c32659262be4340216850fa89e86f3
김광민 <goos2019@naver.com>
2023-10-12 23:22:27 +0900

    [챕터10 24266 문제]

---
+++ stage10/Main_24266.java
@@ -0,0 +1,26 @@
+package stage10;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_24266 {
+    public static void main(String[] args) {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            // MenOfPassion는 삼중 for문
+            // 외곽 n
+            // 내부 n
+            // 중심 n
+            // n^3
+            long n = Long.parseLong(br.readLine());
+
+            System.out.println(n * n * n);
+            System.out.println(3);
+
+        } catch(Exception e) {
+
+        }
+    }
+}
