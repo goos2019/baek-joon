64677746e62c1f1358db0e7be4bd0046b1bf67ae
김광민 <goos2019@naver.com>
2023-10-12 23:21:15 +0900

    [챕터10 24265 문제]

--- stage10/Main_24265.java
+++ stage10/Main_24265.java
@@ -9,15 +9,20 @@ public class Main_24265 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         try {
-            // MenOfPassion는 삼중 for문
-            // 외곽 n
-            // 내부 n
-            // 중심 n
-            // n^3
+            // MenOfPassion는 이중 for문
+            // 외부 for문이 i가 1부터 n-1까지 진행하고 -> n - 1
+            // 내부 for문이 i + 1부터 n까지 진행
+            // 1 -> 0
+            // 2 -> 1 1
+            // 3 -> 2 1
+            // 4 -> 3 2 1
+            // 5 -> 4 3 2 1
+            // (n-1)n / 2
+
             long n = Long.parseLong(br.readLine());
 
-            System.out.println(n * n * n);
-            System.out.println(3);
+            System.out.println(((n - 1) * n) / 2L);
+            System.out.println(2);
 
         } catch(Exception e) {
 
