71e86c332d21b76aa0f2eba840d65d6d931e5e85
김광민 <goos2019@naver.com>
2023-10-15 22:54:53 +0900

    [stage12 1436문제] 생각보다 난이도가 엄청 높았음... 문제를 푸는건 쉬웠는데 메모리 초과때문에 최적화를 엄청 해야함
    1. 사용하는 변수 줄이기
    2. 반복횟수 최소화 -> 이게 중요

---
+++ stage12/Main_1436.java
@@ -0,0 +1,56 @@
+package stage12;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * [영화감독 숌]
+ * 1. 종말의 수란 어떤 수에 6이 적어도 3개 이상 연속으로 들어가는 수
+ * 2. 제일 작은 종말의 수는 666이고, 그 다음으로 큰 수는 1666, 2666, 3666, ....
+ * 3. 첫 번째 영화의 제목은 "세상의 종말 666", 두 번째 영화의 제목은 "세상의 종말 1666" 일반화해서 생각하면, N번째 영화의 제목은 세상의 종말 (N번째로 작은 종말의 수)
+ * 4. N번째 영화의 제목에 들어간 수를 출력하는 프로그램을 작성
+ * [입력]
+ * 1. 첫째 줄에 N이 주어진다. N은 10,000보다 작거나 같은 자연수
+ * [출력]
+ * 1. 첫째 줄에 N번째 영화의 제목에 들어간 수를 출력
+ */
+public class Main_1436 {
+    public static void main(String[] args) {
+
+        // 0 // [0]666 -> n[666]
+        // 0 //        [0]1666 2666 3666 4666 5666
+        // 0 //        6660 6661 6662 6663 6664 6665 6666 6667 6668 6669
+        // 0 //        7666 8666 9666 => 19개
+        // 1 //        10666 11666 12666 13666 14666 15666
+        // 1 //        16660 16661 16662 16663 16664 16665 16666 16667 16668 16669
+        // 1 //        17666 18666 19666 => 19개
+        // -> 공식은 나중에 찾기
+        // [브루트 포스로 진행]
+        // 1. 반복문으로 무한대로 진행
+        // 2. 숫자안에 연결된 666이 있는지 확인
+        // 3. 있으면 count가 증가
+        // 4. 해당 count 발견시 종료
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+
+            short count = (short)Integer.parseInt(br.readLine());
+            int nth = 665;
+
+            while(true) {
+                if(count == 0) break;
+
+                nth += 1;
+
+                String temp = Integer.toString(nth);
+                if(temp.contains("666")) {
+                    count -= 1;
+                }
+            }
+            System.out.println(nth);
+        } catch(Exception e) {
+
+        }
+    }
+}
