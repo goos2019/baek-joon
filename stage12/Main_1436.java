51d0a1a5c4c2644585016a9130e54a12ee490350
김광민 <goos2019@naver.com>
2023-10-16 22:14:34 +0900

    [stage12 1436문제 수정]

--- stage12/Main_1436.java
+++ stage12/Main_1436.java
@@ -24,40 +24,60 @@ public class Main_1436 {
         // 1 //        10666 11666 12666 13666 14666 15666
         // 1 //        16660 16661 16662 16663 16664 16665 16666 16667 16668 16669
         // 1 //        17666 18666 19666 => 19개
+        // 6 //        61666 62666 63666 64666 65666
+        // 6 //        66600 66601 ... 66699
+        // 6 //        67666 68666 69666
+        // 16 //       160 161 162 163 164 165
+        // 16 //       166600 ~ 99
+        // 16 //       167 168 169
+        // 61 //       610 611 612 613 614 615
+        // 61 //       616660
+        // 66 //       660 661 662 663 664 665
+        // 66 //       666000 ... 666999
+        // 66 //       667666 ... 668666 669666
         // -> 공식은 나중에 찾기
-        // [브루트 포스로 진행]
-        // 1. 반복문으로 무한대로 진행
-        // 2. 숫자안에 연결된 666이 있는지 확인
-        // 3. 있으면 count가 증가
-        // 4. 해당 count 발견시 종료
+        // 1. 앞자리수가 증가
+        // 2. 앞자리수의 마지막이 6인경우 뒷자리수를 추가 계산 이때 앞자리수의 6의 개수에 따라 뒷자리의 추가 계산이 증가
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         try {
 
             short N = (short)Integer.parseInt(br.readLine());
-            short count = 0;
-            int num = 666;
+            short count = 1;
             int pre_digit = 0;
             int pre_exp = 0;
             int post_digit = 0;
             int post_exp = 0;
+            int total_digit = 3;
 
             while(true) {
 
-                if(pre_digit%10 == 0) {
-                    pre_exp += 1;
+                if(N == count) {
+                    System.out.println(pre_digit + total_digit + post_digit);
                 }
 
-                if((pre_digit/Math.pow(10, pre_exp))%10 == 6) {
+                count++;
+
+                // 자리수 증감
+                if(pre_digit%Math.pow(10, pre_exp) == 0) {
+                    pre_exp++;
+                    total_digit++;
+                }
+
+                if(post_digit%Math.pow(10, post_exp) == 0) {
+                    post_exp++;
+                    post_digit = 0;
+                }
+
+                if(pre_digit%Math.pow(10, pre_exp) == 6) {
+
+
 
                 } else {
-                    ++pre_digit;
+                    pre_digit++;
                 }
 
-                if(String.valueOf(num).contains("666")) {
-                    ++count;
-                }
             }
 
         } catch(Exception e) {
