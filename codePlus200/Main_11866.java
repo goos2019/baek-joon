86cb895aa957d76a27690f129b9b585539deb67f
김광민 <goos2019@naver.com>
2023-10-24 23:51:01 +0900

    [11866 성공] * k번째가 추출되면 해당 추출된 idx에 뒤에있는 idx가 붙으므로 idx를 증가시키면 안됨

--- codePlus200/Main_11866.java
+++ codePlus200/Main_11866.java
@@ -2,6 +2,7 @@ package codePlus200;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
 
 public class Main_11866 {
 
@@ -11,7 +12,35 @@ public class Main_11866 {
         StringBuilder sb = new StringBuilder();
 
         try {
-            System.out.println();
+            String[] data = br.readLine().split(" ");
+            int N = Integer.parseInt(data[0]);
+            int K = Integer.parseInt(data[1]);
+            LinkedList<Integer> ll = new LinkedList<>();
+
+            sb.append("<");
+            for(int i = 1; i <= N; i++) {
+                ll.add(i);
+            }
+
+            int count = 1;
+            int idx = 0;
+
+            while(ll.size() > 1) {
+
+                if(count == K) {
+                    sb.append(ll.get(idx)).append(", ");
+                    ll.remove(idx);
+                    count = 0;
+                } else {
+                    idx = (idx + 1)%ll.size();
+                }
+                count++;
+            }
+            sb.append(ll.pop());
+            sb.append(">");
+
+
+            System.out.println(sb);
         } catch (Exception e) {
             e.printStackTrace();
         }
