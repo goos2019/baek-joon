98811343297d7e0531ac095304a6a3899982e6e7
김광민 <goos2019@naver.com>
2023-10-24 22:48:24 +0900

    [2164 초기화]

---
+++ codePlus200/Main_2164.java
@@ -0,0 +1,18 @@
+package codePlus200;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_2164 {
+    public static void main(String[] args) {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        try {
+            System.out.println();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
