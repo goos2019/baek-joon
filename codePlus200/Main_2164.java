cddd6a2b364eaeda6b672d111e72afb63496e54b
김광민 <goos2019@naver.com>
2023-10-24 23:03:06 +0900

    [2164 성공]

--- codePlus200/Main_2164.java
+++ codePlus200/Main_2164.java
@@ -2,6 +2,7 @@ package codePlus200;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
 
 public class Main_2164 {
     public static void main(String[] args) {
@@ -10,7 +11,25 @@ public class Main_2164 {
         StringBuilder sb = new StringBuilder();
 
         try {
-            System.out.println();
+            int max = Integer.parseInt(br.readLine());
+            LinkedList<Integer> ll = new LinkedList<>();
+
+            for(int i = 1; i <= max; i++) {
+                ll.add(i);
+            }
+
+            int count = 0;
+
+            while(ll.size() != 1) {
+                if(count%2 == 0) {
+                    ll.removeFirst();
+                } else if(count%2 == 1) {
+                    ll.addLast(ll.poll());
+                }
+                count++;
+            }
+
+            System.out.println(ll.poll());
         } catch(Exception e) {
             e.printStackTrace();
         }
