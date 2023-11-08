199f79de58478557bc67ff9922dc244d476586dc
김광민 <goos2019@naver.com>
2023-11-09 01:31:47 +0900

    [1620 개선] 기존 660ms 1. ArrayList 대신에 일반 배열을 사용 588ms 2. 패턴 비교 대신에 charAt을 이용한 아스키코드 비교 563ms

--- stage14/Main_1620.java
+++ stage14/Main_1620.java
@@ -1,54 +1,33 @@
 package stage14;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
-import java.util.regex.Pattern;
-
 public class Main_1620 {
-
     public static void main(String[] args) {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
         try {
             String[] NM = br.readLine().split(" ");
             int N = Integer.parseInt(NM[0]);
             int M = Integer.parseInt(NM[1]);
-            ArrayList<String> list = new ArrayList<>();
+            String[] list = new String[N + 1];
             Map<String, Integer> mapList = new HashMap<>();
-            Pattern pattern = Pattern.compile("[0-9]+");
 
             for(int i = 0; i < N; i++) {
-                String data = br.readLine();
-                list.add(data);
-                mapList.put(data, i + 1);
+                list[i + 1] = br.readLine();
+                mapList.put(list[i + 1], i + 1);
             }
 
             for(int i = 0; i < M; i++) {
                 String data = br.readLine();
-
-                if(pattern.matcher(data).matches()) {
-                    sb.append(list.get(Integer.parseInt(data) - 1)).append('\n');
-                } else {
+                if((int)data.charAt(0) >= 65) {
                     sb.append(mapList.get(data)).append('\n');
+                } else {
+                    sb.append(list[Integer.parseInt(data)]).append('\n');
                 }
             }
 
             System.out.println(sb);
-
-//            Pattern pattern = Pattern.compile("[0-9]+");
-//            while(true) {
-//                String data = br.readLine();
-//                Matcher matcher = pattern.matcher(data);
-//
-//                System.out.println(matcher.matches());
-//                while (matcher.find()) {
-//                    System.out.println("찾음");
-//                }
-//                System.out.println(Pattern.matches("[0-9]+", "123456"));
-//            }
 
         } catch(Exception e) {
             e.printStackTrace();
