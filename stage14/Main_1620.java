7f64717ebad0b3b28a984e3dbcd3d49c2ae0b846
김광민 <goos2019@naver.com>
2023-11-09 13:01:52 +0900

    [1620 개선] 기존 660ms 1. ArrayList 대신에 일반 배열을 사용 588ms 2. 패턴 비교 대신에 charAt을 이용한 아스키코드 비교 563ms

--- stage14/Main_1620.java
+++ stage14/Main_1620.java
@@ -10,25 +10,24 @@ public class Main_1620 {
             String[] NM = br.readLine().split(" ");
             int N = Integer.parseInt(NM[0]);
             int M = Integer.parseInt(NM[1]);
-            String[] list = new String[N + 1];
-            Map<String, Integer> mapList = new HashMap<>();
-
-            for(int i = 0; i < N; i++) {
-                list[i + 1] = br.readLine();
-                mapList.put(list[i + 1], i + 1);
+            String[] dogam = new String[N];
+            Map<String, Integer> dogamMap = new HashMap<String, Integer>();
+            for (int i = 0; i < dogam.length; i++) {
+                dogam[i] = br.readLine();
+                dogamMap.put(dogam[i], i);
             }
-
-            for(int i = 0; i < M; i++) {
-                String data = br.readLine();
-                if((int)data.charAt(0) >= 65) {
-                    sb.append(mapList.get(data)).append('\n');
-                } else {
-                    sb.append(list[Integer.parseInt(data)]).append('\n');
+            while(M-->0) {
+                String str = br.readLine();
+                if((int)str.charAt(0)>=65) {
+                    //문자
+                    sb.append(dogamMap.get(str)+1).append("\n");
+                }else {
+                    //숫자
+                    int idx = Integer.parseInt(str);
+                    sb.append(dogam[idx-1]).append("\n");
                 }
             }
-
             System.out.println(sb);
-
         } catch(Exception e) {
             e.printStackTrace();
         }
