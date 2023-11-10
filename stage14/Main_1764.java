2293ac7fa8f553aa2b79ce6cddeb0054a4f78cc6
김광민 <goos2019@naver.com>
2023-11-11 00:12:08 +0900

    [1764 성공]

---
+++ stage14/Main_1764.java
@@ -0,0 +1,63 @@
+package stage14;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main_1764 {
+
+    public static void main(String[] args) {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        try {
+            String[] NM = br.readLine().split(" ");
+            int N = Integer.parseInt(NM[0]);
+            int M = Integer.parseInt(NM[1]);
+            Map<String, Integer> list = new HashMap<>();
+
+            for(int i = 0; i < N; i++) {
+                list.put(br.readLine(), 0);
+            }
+
+            for(int i = 0; i < M; i++) {
+                String longTimeNoSee = br.readLine();
+                if(list.get(longTimeNoSee) == null) {
+                    list.remove(longTimeNoSee);
+                } else {
+                    int count = list.get(longTimeNoSee);
+                    list.put(longTimeNoSee, ++count);
+                }
+            }
+
+            ArrayList<String> member = new ArrayList<>(list.keySet());
+
+            for(String name: member) {
+                if(list.get(name) == 0) {
+                    list.remove(name);
+                }
+            }
+
+            member = new ArrayList<>(list.keySet());
+
+            member.sort(new Comparator<String>() {
+                @Override
+                public int compare(String o1, String o2) {
+                    return o1.compareTo(o2);
+                }
+            });
+
+            sb.append(member.size()).append('\n');
+
+            for(String name: member) {
+                sb.append(name).append('\n');
+            }
+
+            System.out.println(sb);
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
