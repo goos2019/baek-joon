32e5e755621ec10a8f4994d46cc3867634424e6b
김광민 <goos2019@naver.com>
2023-11-11 23:15:19 +0900

    [1269 개선]
    1. HashSet의 해싱을 통한 빠른검색을 활용 2440ms -> 660ms

--- stage14/Main_1269.java
+++ stage14/Main_1269.java
@@ -2,8 +2,7 @@ package stage14;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
 
 public class Main_1269 {
 
@@ -15,86 +14,30 @@ public class Main_1269 {
             String[] AB = br.readLine().split(" ");
             int A = Integer.parseInt(AB[0]);
             int B = Integer.parseInt(AB[1]);
-            String[] aData = br.readLine().split(" ");
-            String[] bData = br.readLine().split(" ");
 
-            Arrays.sort(aData, new Comparator<String>() {
-                @Override
-                public int compare(String o1, String o2) {
-                    return Integer.parseInt(o1) - Integer.parseInt(o2);
-                }
-            });
+            Set<Integer> aData = new HashSet<>();
+            Set<Integer> bData = new HashSet<>();
 
-            Arrays.sort(bData, new Comparator<String>() {
-                @Override
-                public int compare(String o1, String o2) {
-                    return Integer.parseInt(o1) - Integer.parseInt(o2);
-                }
-            });
+            for (String data : br.readLine().split(" ")) {
+                aData.add(Integer.parseInt(data));
+            }
+
+            for (String data : br.readLine().split(" ")) {
+                bData.add(Integer.parseInt(data));
+            }
 
             int count = 0;
 
-            for(String a :aData) {
-                if(getIndex(bData, a)) {
+            for (int value : aData) {
+                if (aData.contains(value)) {
                     ++count;
                 }
             }
 
-            System.out.println(aData.length + bData.length - (count * 2));
+            System.out.println(aData.size() + bData.size() - (count * 2));
 
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
-        }
-    }
-
-    public static boolean getIndex(String[] data, String target) {
-
-        int value = Integer.parseInt(target);
-        int minValue = Integer.parseInt(data[0]);
-        int maxValue = Integer.parseInt(data[data.length - 1]);
-
-        if(value < minValue || value > maxValue) {
-
-            return false;
-
-        } else if(value == minValue || value == maxValue) {
-
-            return true;
-
-        } else {
-
-            int lowLength = 0;
-            int maxLength = data.length;
-            int targetLength = data.length/2;
-
-            while(true) {
-
-                int compareValue = Integer.parseInt(data[targetLength]);
-
-                if(value == compareValue) {
-                    return true;
-                } else if(value > compareValue) {
-
-                    lowLength = targetLength;
-                    int newTargetLength = lowLength + (maxLength - lowLength)/2;
-
-                    if(newTargetLength == targetLength || newTargetLength >= maxLength || newTargetLength <= lowLength) {
-                        return false;
-                    } else {
-                        targetLength = newTargetLength;
-                    }
-                } else {
-
-                    maxLength = targetLength;
-                    int newTargetLength = lowLength + (maxLength - lowLength)/2;
-
-                    if(newTargetLength == targetLength || newTargetLength >= maxLength || newTargetLength <= lowLength) {
-                        return false;
-                    } else {
-                        targetLength = newTargetLength;
-                    }
-                }
-            }
         }
     }
 }
