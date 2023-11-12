b7ccf5683c29695a2d153a7c53e095b0fe0cdc22
김광민 <goos2019@naver.com>
2023-11-12 22:04:01 +0900

    [1934 개선] 알고리즘 개선으로 속도 개선 344ms -> 152ms 1. 유클리드 호제법 사용해서 최대공약수를 구함 2. 최대공약수로 최소공배수를 구함

--- stage15/Main_1934.java
+++ stage15/Main_1934.java
@@ -15,21 +15,13 @@ public class Main_1934 {
             for(int i = 0; i < count; i++) {
                 String[] line = br.readLine().split(" ");
 
-                int highCoef = Math.max(Integer.parseInt(line[0]), Integer.parseInt(line[1]));
-                int lowCoef = Math.min(Integer.parseInt(line[0]), Integer.parseInt(line[1]));
-                int A = highCoef;
-                int B = lowCoef;
+                int a = Math.max(Integer.parseInt(line[0]), Integer.parseInt(line[1]));
+                int b = Math.min(Integer.parseInt(line[0]), Integer.parseInt(line[1]));
 
-                while(true) {
-                    B += lowCoef;
-                    if(B%A == 0) {
-                        break;
-                    } else if(B > A) {
-                        A += highCoef;
-                    }
-                }
+                // lcm(a, b) = |ab| / gcd(a, b)
+                int gcd = gcdEuclideanAlgorithm(a, b);
 
-                sb.append(B).append('\n');
+                sb.append(Math.abs(a * b)/gcd).append('\n');
             }
 
             System.out.println(sb);
@@ -37,5 +29,13 @@ public class Main_1934 {
         } catch(Exception e) {
             e.printStackTrace();
         }
+    }
+
+    // if a > b, a = bq + r (0 <= r < b)
+    // gcd(a, b) = gcd(b, r)
+    // r=0 then gcd is b
+    public static int gcdEuclideanAlgorithm(int A, int B) {
+        if(B == 0) return A;
+        return gcdEuclideanAlgorithm(B, A%B);
     }
 }
