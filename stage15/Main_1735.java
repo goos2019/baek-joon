d5bf6f3096cda75d92f1503dc97dd242dbcd7ae0
김광민 <goos2019@naver.com>
2023-11-13 23:13:39 +0900

    [1735 개선] 기약분수를 봐서 기존 식을 마개조해서 한거라 가독성이나 능률이 많이 떨어짐 1. 두 분수의 합을 구하는 간단한 식으로 보정 2. 기존에 쓸데없는 기능 삭제

--- stage15/Main_1735.java
+++ stage15/Main_1735.java
@@ -1,7 +1,6 @@
 package stage15;
 
 import java.io.*;
-import java.util.*;
 
 public class Main_1735 {
 
@@ -12,27 +11,15 @@ public class Main_1735 {
         try {
             String[] data1 = br.readLine().split(" ");
             String[] data2 = br.readLine().split(" ");
-            int A1, A2, B1, B2 = 0;
+            int A1 = Integer.parseInt(data1[0]);
+            int B1 = Integer.parseInt(data1[1]);
+            int A2 = Integer.parseInt(data2[0]);
+            int B2 = Integer.parseInt(data2[1]);
 
-            if(Integer.parseInt(data1[1]) > Integer.parseInt(data2[1])) {
-                A1 = Integer.parseInt(data1[0]);
-                B1 = Integer.parseInt(data1[1]);
-                A2 = Integer.parseInt(data2[0]);
-                B2 = Integer.parseInt(data2[1]);
-            } else {
-                A1 = Integer.parseInt(data2[0]);
-                B1 = Integer.parseInt(data2[1]);
-                A2 = Integer.parseInt(data1[0]);
-                B2 = Integer.parseInt(data1[1]);
-            }
+            int A = (A1 * B2) + (A2 * B1);
+            int B = B1 * B2;
 
-            int gcd = gcdEA(B1, B2);
-
-            // lcm(a, b) = |a * b| / gcd(a, b)
-            int lcm = Math.abs(B1 * B2) / gcd;
-            int sum = A1 * (lcm/B1) + A2 * (lcm/B2);
-
-            int[] data = divValue(sum, lcm);
+            int[] data = divValue(A, B);
 
             System.out.println(data[0] + " " + data[1]);
 
