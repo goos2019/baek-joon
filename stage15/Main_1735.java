4356405db7c03d99c9ec5bc759607f7f39840f4e
김광민 <goos2019@naver.com>
2023-11-13 23:06:48 +0900

    [1735 성공] 기약분수를 못봐서 틀림... 두수의 합만 봤음

--- stage15/Main_1735.java
+++ stage15/Main_1735.java
@@ -32,7 +32,9 @@ public class Main_1735 {
             int lcm = Math.abs(B1 * B2) / gcd;
             int sum = A1 * (lcm/B1) + A2 * (lcm/B2);
 
-            System.out.println(sum + " " + lcm);
+            int[] data = divValue(sum, lcm);
+
+            System.out.println(data[0] + " " + data[1]);
 
         } catch(Exception e) {
             e.printStackTrace();
@@ -45,5 +47,28 @@ public class Main_1735 {
     static int gcdEA(int a, int b) {
         if(b == 0) return a;
         return gcdEA(b, a%b);
+    }
+
+    static int[] divValue(int a, int b) {
+
+        int gcd = 0;
+
+        if(a > b) {
+            while(true){
+                gcd = gcdEA(a, b);
+                if(gcd == 1) break;
+                a /= gcd;
+                b /= gcd;
+            }
+        } else {
+            while(true){
+                gcd = gcdEA(b, a);
+                if(gcd == 1) break;
+                a /= gcd;
+                b /= gcd;
+            }
+        }
+
+        return new int[]{a, b};
     }
 }
