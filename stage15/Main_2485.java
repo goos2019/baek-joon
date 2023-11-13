4768ecdece4555f88437e869e97ccb82548490e2
김광민 <goos2019@naver.com>
2023-11-14 00:29:20 +0900

    [2485 정답]

---
+++ stage15/Main_2485.java
@@ -0,0 +1,47 @@
+package stage15;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_2485 {
+
+    public static void main(String[] args) {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            int count = Integer.parseInt(br.readLine());
+            int first = Integer.parseInt(br.readLine());
+            int before = Integer.parseInt(br.readLine());
+            int last = 0;
+            int gcd = 0;
+
+            for(int i = 2; i < count; i++) {
+                int value = Integer.parseInt(br.readLine());
+                int afterGcd = gcd(before - first, value - first);
+                before = value;
+                if(gcd == 0) {
+                    gcd = afterGcd;
+                } else if(gcd > afterGcd) {
+                    gcd = gcd(gcd, afterGcd);
+                } else {
+                    gcd = gcd(afterGcd, gcd);
+                }
+
+                if(i == (count - 1)) {
+                    last = value;
+                }
+            }
+
+            System.out.println((int)Math.ceil(last/(gcd*1.0)) - count - (int)Math.ceil((first - gcd)/(gcd*1.0)));
+
+        } catch(Exception e) {
+
+        }
+    }
+
+    static int gcd(int a, int b) {
+        if(b == 0) return a;
+        return gcd(b, a%b);
+    }
+}
