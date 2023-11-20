4a1a68c3bff08653149ab67558c37931064d5329
김광민 <goos2019@naver.com>
2023-11-20 23:33:14 +0900

    [25192 실패]

---
+++ stage20/Main_25192.java
@@ -0,0 +1,33 @@
+package stage20;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main_25192 {
+
+    public static void main(String[] args) {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            int count = Integer.parseInt(br.readLine());
+            int gomgom = 0;
+            Set<String> set = new HashSet<String>();
+
+            for(int i = 0; i < count; i++) {
+                String name = br.readLine();
+                if(!name.equals("ENTER") && !set.contains(name)) {
+                    gomgom++;
+                    set.add(name);
+                }
+            }
+
+            System.out.println(gomgom);
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
