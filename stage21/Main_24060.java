9559ad52e605a627328083503740b5687651cdfd
김광민 <goos2019@naver.com>
2023-11-27 21:44:40 +0900

    [24060 틀림]

--- stage21/Main_24060.java
+++ stage21/Main_24060.java
@@ -5,6 +5,7 @@ import java.io.*;
 public class Main_24060 {
 
     static int[] A;
+    static int count = 0;
 
     public static void main(String[] args) {
 
@@ -15,6 +16,7 @@ public class Main_24060 {
             String[] data = br.readLine().split(" ");
             int N = Integer.parseInt(NK[0]);
             int K = Integer.parseInt(NK[1]);
+            count = K;
             A = new int[N];
 
             for(int i = 0; i < N; i++) {
@@ -22,10 +24,6 @@ public class Main_24060 {
             }
 
             merge_sort(0, N - 1);
-
-            for(int value : A) {
-                System.out.println(value);
-            }
 
         } catch(Exception e) {
             e.printStackTrace();
@@ -45,8 +43,9 @@ public class Main_24060 {
         int i = p;
         int j = q + 1;
         int t = 0;
-        int[] tmp = new int[A.length + 1];
-        while(i <= q && j < r) {
+        int[] tmp = new int[A.length];
+
+        while(i <= q && j <= r) {
             if(A[i] <= A[j]) {
                 tmp[t++] = A[i++];
             } else {
@@ -58,10 +57,12 @@ public class Main_24060 {
         }
         while(j <= r) {
             tmp[t++] = A[j++];
-            i = p;
-            t = 1;
         }
+        i = p;
+        t = 0;
         while(i <= r) {
+            --count;
+            if(count == 0) System.out.println(tmp[t]);
             A[i++] = tmp[t++];
         }
     }
