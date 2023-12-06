c4aa6076939d78367879bc86179fab2cbdeb8657
김광민 <goos2019@naver.com>
2023-12-06 23:27:02 +0900

    [2743 성공]

---
+++ under6/Main_2743.java
@@ -0,0 +1,16 @@
+package under6;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_2743 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            String str = br.readLine();
+            System.out.println(str.length());
+        } catch(Exception e) {
+
+        }
+    }
+}
