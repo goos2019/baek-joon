47d803ed4e0f76b26d72a5fc5621a1e0789f2a0c
김광민 <goos2019@naver.com>
2024-06-06 00:32:17 +0900

    [15649 성공] 속도가 너무 느림

--- stage22/Main_15649.java
+++ stage22/Main_15649.java
@@ -5,8 +5,8 @@ import java.io.InputStreamReader;
 
 public class Main_15649 {
 
-    static String[] data;
-    static boolean[] data2;
+    static int[] valueArray;
+    static boolean[] visit;
 
     public static void main(String[] args) {
 
@@ -16,19 +16,34 @@ public class Main_15649 {
             String[] NM = br.readLine().split(" ");
             int N = Integer.parseInt(NM[0]);
             int M = Integer.parseInt(NM[1]);
-            data = new String[999999];
-            NM(N, M, 1, 1, 0);
+            valueArray = new int[M];
+            visit = new boolean[N];
+            Main_15649 main = new Main_15649();
+            main.NM(N, M, 0);
         } catch(Exception e) {
             e.printStackTrace();
         }
     }
 
-    static void NM(int N, int M, int start, int end, int line) {
-        if(start == N) return;
+    public void NM(int N, int M, int depth) {
 
-        NM(N, M, ++start, end, line);
-        System.out.print(start);
-        NM(N, M, start, ++end, line);
-        System.out.println(end);
+        if(depth == M) {
+            for(int value : valueArray) {
+                System.out.print(value + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for(int i = 0; i < N; i++) {
+
+            if(visit[i] == false) {
+
+                visit[i] = true;
+                valueArray[depth] = i + 1;
+                NM(N, M, depth + 1);
+                visit[i] = false;
+            }
+        }
     }
 }
